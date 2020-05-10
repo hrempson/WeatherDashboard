c1005f94355cc0c9cfcb8fa67453767dafcfa71e
Heather Empson <hrempson@gmail.com>
2020-05-09 21:01:09 -0700

    finished functions

--- assets/script.js
+++ assets/script.js
@@ -3,95 +3,165 @@ var lat = null;
 var lon = null;
 var apiKey = 'b0a1c026a3e75b564efaab3bbd39bf04';
 var city = null;
+var searchHistory = [];
 
-$ ('.btn').on ('click', function (event) {
-  event.preventDefault ();
-  city = $ ('.search').val ();
-  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+// When page loads check for search history
+$(document).ready(function () {
+    readSearchHistory();
+});
 
-  // AJAX call to get weather info
-  console.log (city);
-  console.log (queryURL);
-  $.ajax ({
-    url: queryURL,
-    method: 'GET',
-  })
-    // Store the retrieved data in "response"
-    .then (function (response) {
-      console.log (queryURL);
-      console.log (response);
+$('.btn').on('click', doSearch);
+$('.history').on('click', doSearch);
 
-      var iconURL = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-      var date = response.dt;
+function doSearch(event) {
+    event.preventDefault();
+    city = $('.search').val();
 
-      // Transfer content to HTML
-      $ ('.city').html ('<h1>' + response.name + '</h1>');
-      $ ('.date').text (date.toString ());
-      $ ('.icon').attr ('src', iconURL);
-      $ ('.humidity').text ('Humidity: ' + response.main.humidity);
-      $ ('.wind').text ('Wind Speed: ' + response.wind.speed);
-      $ ('.tempF').text ('Temperature (F) ' + tempF.toFixed (2));
+    // Make sure the search is not blank
+    if (city.length > 0) {
+        addSearchHistory(city);
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        $('.search').val('');
 
-      lon = response.coord.lon;
-      lat = response.coord.lat;
+        // AJAX call to get weather info
+        console.log(city);
+        console.log(queryURL);
+        $.ajax({
+                url: queryURL,
+                method: 'GET',
+            })
+            // Store the retrieved data in "response"
+            .then(function (response) {
+                console.log(queryURL);
+                console.log(response);
 
-      // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}
-      if (lon === null || lat === null) {
-        throw new Error (`lon=${lon} && lat=${lat} one of these is null`);
-      }
-      var uvURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${apiKey}`;
-      $.ajax ({
-        url: uvURL,
-        method: 'GET',
-      }).then (function (response) {
-        var uvi = response.current.uvi;
-        // Using Wikipedia Ultraviolet Index https://en.wikipedia.org/wiki/Ultraviolet_index
-        if (uvi <= 3) {
-          $ ('.uviValue').addClass ('badge-success');
-          $ ('.uviValue').removeClass ('badge-danger');
-          $ ('.uviValue').removeClass ('badge-warning');
-        } else if (uvi > 3 && uvi < 6) {
-          $ ('.uviValue').addClass ('badge-warning');
-          $ ('.uviValue').removeClass ('badge-danger');
-          $ ('.uviValue').removeClass ('badge-success');
-        } else {
-          $ ('.uviValue').removeClass ('badge-warning');
-          $ ('.uviValue').addClass ('badge-danger');
-          $ ('.uviValue').removeClass ('badge-success');
-        }
+                var iconURL = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+                var date = response.dt;
 
-        $ ('.uviValue').text (uvi);
-      });
+                // Transfer content to HTML
+                $('.city').html('<h1>' + response.name + '</h1>');
+                //   $ ('.date').text (date.toString ());
+                $('.date').text(unixTimestampToDate(date));
+                $('.icon').attr('src', iconURL);
+                $('.humidity').text('Humidity: ' + response.main.humidity + '%');
+                $('.wind').text('Wind Speed: ' + response.wind.speed);
+                $('.tempF').text('Temperature (F) ' + tempF.toFixed(2));
 
-      var fiveDayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current&appid=${apiKey}`;
+                lon = response.coord.lon;
+                lat = response.coord.lat;
 
-      if (lon === null || lat === null) {
-        throw new Error (`lon=${lon} && lat=${lat} one of these is null`);
-      }
+                // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}
+                if (lon === null || lat === null) {
+                    throw new Error(`lon=${lon} && lat=${lat} one of these is null`);
+                }
+                var uvURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${apiKey}`;
+                $.ajax({
+                    url: uvURL,
+                    method: 'GET',
+                }).then(function (response) {
+                    var uvi = response.current.uvi;
+                    // Using Wikipedia Ultraviolet Index https://en.wikipedia.org/wiki/Ultraviolet_index
+                    if (uvi <= 3) {
+                        $('.uviValue').addClass('badge-success');
+                        $('.uviValue').removeClass('badge-danger');
+                        $('.uviValue').removeClass('badge-warning');
+                    } else if (uvi > 3 && uvi < 6) {
+                        $('.uviValue').addClass('badge-warning');
+                        $('.uviValue').removeClass('badge-danger');
+                        $('.uviValue').removeClass('badge-success');
+                    } else {
+                        $('.uviValue').removeClass('badge-warning');
+                        $('.uviValue').addClass('badge-danger');
+                        $('.uviValue').removeClass('badge-success');
+                    }
 
-      $.ajax ({
-        url: fiveDayURL,
-        method: 'GET',
-      }).then (function (response) {
-        // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
+                    $('.uviValue').text(uvi);
+                });
 
-        // for loop to create Daily Weather Cards...
-        // TODO: Bootstrap Grid System, or Flexbox layout...
-        // Template literals with multiple lines https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-        for (var i = 0; i < response.daily.length; i++) {
-          var iconURL = `http://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}@2x.png`;
-          var temp = (response.daily[i].temp.day - 273.15) * 1.80 + 32;
-          $ ('.container-5-day').append (`
-        <div class="card">
+                var fiveDayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current&appid=${apiKey}`;
+
+                if (lon === null || lat === null) {
+                    throw new Error(`lon=${lon} && lat=${lat} one of these is null`);
+                }
+
+                $.ajax({
+                    url: fiveDayURL,
+                    method: 'GET',
+                }).then(function (response) {
+                    // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
+                    $('.container-5-day').html('');
+                    // for loop to create Daily Weather Cards...
+                    // TODO: Bootstrap Grid System, or Flexbox layout...
+                    // Template literals with multiple lines https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+                    for (var i = 1; i < 6; i++) {
+                        var iconURL = `http://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}@2x.png`;
+                        var temp = (response.daily[i].temp.day - 273.15) * 1.80 + 32;
+                        console.log(fiveDayURL);
+                        $('.container-5-day').append(`
+        <div class="col-sm card bg-primary mr-10">
             <div class="card-body">
-                <p class="card-title">${response.daily[i].dt}</p>
+                <p class="card-title">${unixTimestampToDate (response.daily[i].dt)}</p>
                 <img src="${iconURL}">
                 <p class="card-text">Temp: ${Number.parseFloat (temp).toPrecision (4)}</p>
-                <p class="card-text">Humidity: ${response.daily[i].humidity}</p>
+                <p class="card-text">Humidity: ${response.daily[i].humidity}%</p>
             </div>
         </div>`);
-        }
-      });
-    });
-});
+                    }
+                });
+            });
+    }
+}
+
+// wrapper for search history click
+function historyClick(event) {
+    $('.search').val(window.event.srcElement.innerText);
+    doSearch(window.event);
+}
+
+// Add a new Search history to UI and save history
+function addSearchHistory(city) {
+    // If city is in history don't add again.
+    if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        drawHistoryItem(city)
+        saveSearchHistory();
+    } else {
+      // Sort last search to first position.
+      console.log(`searchHistory Array: ${searchHistory}`);
+      var newSearchHistory = searchHistory.filter(word => word != city);
+      console.log(`newSearchHistory Array: ${newSearchHistory}`);
+      newSearchHistory.unshift(city);
+      console.log(`newSearchHistory unshifted Array: ${newSearchHistory}`);
+      searchHistory = newSearchHistory;
+    }
+}
+
+// Save search history to local storage
+function saveSearchHistory() {
+    localStorage.setItem('history', JSON.stringify(searchHistory));
+}
+
+// Reading history from localstorage
+function readSearchHistory() {
+    var history = JSON.parse(localStorage.getItem('history'));
+    for (var i = 0; i < history.length; i++) {
+        drawHistoryItem(history[i]);
+    }
+}
+
+function drawHistoryItem(value) {
+    $('.history-container').prepend(`
+  <tr class="history">
+  <th scope="row" onclick="historyClick()">${value}</th>
+  </tr> `);
+}
+
+// Utility function to convert from UNIX timestamp to JavaScript date
+function unixTimestampToDate(seconds) {
+    var date = new Date(seconds * 1000);
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    return `${month}/${day}/${year}`;
+}
